33d2cad8cdba9554a75b1b2446ca8ce2eea13aa0
Miguel Rodriguez <miguekos1233@gmail.com>
2020-10-29 19:40:53 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,52 @@
+from doctest import debug
+
+from flask import Flask, render_template, make_response, request, send_from_directory
+import pdfkit
+import pytz
+from datetime import datetime
+import qrcode  # Importamos el modulo necesario para trabajar con codigos QR
+
+app = Flask(__name__)
+app.config['PDF_FOLDER'] = 'fileserver/'
+app.config['TEMPLATE_FOLDER'] = 'templates/'
+
+@app.route('/fileserver/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['PDF_FOLDER'],
+                               filename)
+
+@app.route('/ticketpdf', methods=['POST'])
+def index():
+    lima = pytz.timezone('America/Lima')
+    li_time = datetime.now(lima)
+    _json = request.json
+    _json['registro']['registro'] = int(_json['registro']['registro'])
+    _json['registro']['created_at'] = "{}".format(_json['registro']['created_at'])
+    id = _json['id']
+    imagen = qrcode.make("{}".format(id))
+    archivo_imagen = open(app.config['PDF_FOLDER'] + '{}.png'.format(_json['registro']['registro']), 'wb')
+    imagen.save(archivo_imagen)
+    archivo_imagen.close()
+    pdffile = app.config['PDF_FOLDER'] + '{}.pdf'.format(_json['registro']['registro'])
+    # path_wkthmltopdf = 'wkhtmltox/bin/wkhtmltopdf.exe'
+    # config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
+    rendered = render_template('test.html', json=_json,
+                               pdf="http://127.0.0.1:5454/fileserver/uploads/{}.png".format(_json['registro']['registro']),
+                               qr="http://127.0.0.1:5454/fileserver/uploads/{}.png".format(
+                                   _json['registro']['registro']))
+    # css = ['estado.css']
+    pdf = pdfkit.from_string(rendered, pdffile)
+    # pdf = pdfkit.from_string(rendered, False, css=css, configuration=config)
+    # pdf = pdfkit.from_string(rendered, pdffile, configuration=config)
+
+    return '''Click here to open the
+        <a href="http://localhost:5000/static/pdf/demo4.pdf">pdf</a>.'''
+
+    # response = make_response(pdf)
+    # response.headers['Content-Type'] = 'aplication/pdf'
+    # response.headers['Content-Disposition'] = 'attachment; filename=outout.pdf'
+    # print(response)
+    # return response
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5454, host="0.0.0.0")
---
+++ main.py
@@ -0,0 +1,34 @@
+from xhtml2pdf import pisa             # import python module
+
+
+# from xhtml2pdf.config.httpconfig import httpConfig
+# httpConfig.save_keys('nosslcheck', True)
+# pisaStatus = pisa.CreatePDF(
+#         sourceHtml,
+#         dest=resultFile)
+
+
+# Define your data
+source_html = "{}".format("<html><body><p>To PDF or not to PDF</p></body></html>")
+output_filename = "test.pdf"
+
+# Utility function
+def convert_html_to_pdf(source_html, output_filename):
+    # open output file for writing (truncated binary)
+    result_file = open(output_filename, "w+b")
+
+    # convert HTML to PDF
+    pisa_status = pisa.CreatePDF(
+            source_html,                # the HTML to convert
+            dest=result_file)           # file handle to recieve result
+
+    # close output file
+    result_file.close()                 # close output file
+
+    # return False on success and True on errors
+    return pisa_status.err
+
+# Main program
+if __name__ == "__main__":
+    pisa.showLogging()
+    convert_html_to_pdf(source_html, output_filename)
