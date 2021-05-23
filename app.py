d4b58f5c40a5d3584e45f3f67df29ec3a69aa1b7
Miguel Rodriguez <miguekos1233@gmail.com>
2021-05-23 17:37:25 -0500

    pie

--- app.py
+++ app.py
@@ -57,7 +57,6 @@ options = {
     'no-outline': None
 }
 
-
 @app.route('/gnrpdf/fileserver/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['PDF_FOLDER'],
@@ -67,7 +66,6 @@ def uploaded_file(filename):
 def uploaded_file_static(filename):
     return send_from_directory(app.config['STATIC'],
                                filename)
-
 
 @app.route('/imprimirticketpdf', methods=['POST'])
 def index():
@@ -127,7 +125,62 @@ def actadeservicios(orden, tipo):
                 js_servic = response['operac'][0]['f_js_acta_operac']['js_servic']
                 print("js_servic", js_servic)
                 js_articu = response['operac'][0]['f_js_acta_operac']['js_articu']
-                rendered = render_template('actadeservicios.html', orden=orden, json=response['operac'], js_servic=js_servic if js_servic else [], js_articu=js_articu if js_articu else [], fecha=fechaactual)
+                rendered = render_template('actadeservicios_reinventing.html', orden=orden, json=response['operac'], js_servic=js_servic if js_servic else [], js_articu=js_articu if js_articu else [], fecha=fechaactual)
+
+                if tipo == "1":
+                    pdf = pdfkit.from_string(rendered, False, options=options) if os.name != "nt" else pdfkit.from_string(
+                        rendered, False, options=options, configuration=config)
+                    response = make_response(pdf)
+                    response.headers['Content-Type'] = 'aplication/pdf'
+                    response.headers['Content-Disposition'] = 'attachment; filename=actadeservicios_{}.pdf'.format(orden)
+                    return response
+                if tipo == "2":
+                    pdfkit.from_string(rendered, pdffile, options=options) if os.name != "nt" else pdfkit.from_string(
+                        rendered, pdffile, options=options, configuration=config)
+                    return {
+                        "codRes": "00",
+                        "message": "{}/gnrpdf/fileserver/{}.pdf".format("http://127.0.0.1:5238", orden)
+                    }
+                # pdf = pdfkit.from_string(rendered, pdffile, options=options, configuration=config)
+
+                # return "http://95.111.235.214:5238/fileserver/tickets/{}.pdf".format(_json['registro']['registro'])
+                # return "http://127.0.0.1:5238/fileserver/{}.pdf".format("prueba")
+
+        else:
+            return "Error Controlado"
+    except ValueError:
+        print(ValueError)
+        return {
+            "codRes": "99",
+            "message": "Error Controlado"
+        }
+
+
+
+@app.route('/gnrpdf/formulario/<orden>/<tipo>', methods=['GET'])
+def formulario_reinventing(orden, tipo):
+    try:
+        print(orden)
+        url = 'https://api.reinventing.com.pe/v2.0/pdf/js_acta_operac/{}'.format(orden)
+        headers = {'content-type': 'application/json'}
+        # body = {
+        #     "id": orden
+        # }
+        # data = json.dumps(body)
+        x = requests.get(url, headers=headers)
+        response = json.loads(x.content)
+        print("cuenta", response)
+        if response['res'] == "ok":
+            d = 1
+            if d == 1:
+                pdffile = app.config['PDF_FOLDER'] + '{}.pdf'.format(orden)
+                lima = pytz.timezone('America/Lima')
+                fechaactual = current_date_format(datetime.now(lima))
+                print(fechaactual)
+                js_servic = response['operac'][0]['f_js_acta_operac']['js_servic']
+                print("js_servic", js_servic)
+                js_articu = response['operac'][0]['f_js_acta_operac']['js_articu']
+                rendered = render_template('formulario_reinventing.html', orden=orden, json=response['operac'], js_servic=js_servic if js_servic else [], js_articu=js_articu if js_articu else [], fecha=fechaactual)
 
                 if tipo == "1":
                     pdf = pdfkit.from_string(rendered, False, options=options) if os.name != "nt" else pdfkit.from_string(
